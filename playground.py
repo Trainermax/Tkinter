fd5c03d9431b60cd7f674bf116acd6831375937d
PDHS lab <pdhslab@DSUSD.K12.CA.US>
2017-01-12 10:09:07 -0800

    fixed scrollbar and a lot new stuf

--- playground.py
+++ playground.py
@@ -1,5 +1,6 @@
 from Tkinter import * #gives us access to everything in the Tkinter class
 import tkMessageBox 
+from PIL import Image, ImageTk
 
 
 def buttonpress():
@@ -9,16 +10,53 @@ def buttonpress():
 
 def addtolist():
     entrytxt = entry1.get()
-    listbox1.insert(END, entrytxt)
+    if checkdupe() == False:
+        listbox1.insert(END, entrytxt)
+        size()
     entry1.delete(0, END)
     
 def addtolist2(event):
     entrytxt =entry1.get()
-    listbox1.insert(END, entrytxt)
+    if checkdupe() == False:
+        listbox1.insert(END, entrytxt)
+        size()
     entry1.delete(0, END)
     
 def clearlist(event):
     listbox1.delete(0, END)
+    size()
+    
+def clearlist2():
+    listbox1.delete(0, END)
+    size()
+    
+def checkdupe():
+    name = listbox1.get(0, END)
+    if entry1.get() in name:
+        return True
+    else:
+        return False
+
+def size():
+    label1.config(text=listbox1.size())
+    
+def openfileR():
+    clearlist2()
+    f = open("Readme.txt", "r")
+    for line in f:
+
+        name = line[0:-1]
+        listbox1.insert(END, name)
+    f.close()
+    size()
+
+def openfileW():
+    f = open("Readme.txt", 'w')
+    names = listbox1.get(0,END)
+    for i in names:    
+        f.write(i + "\n") 
+    
+    f.close()
     
 
 root = Tk() #gives us a blank canvas object to work with
@@ -34,14 +72,38 @@ entry1.bind("<Return>", addtolist2)
 label1 = Label(root, text ="Suck my 7inch", bg="pink", anchor=W)
 label1.grid(row=0, column=0, sticky=EW, columnspan=2)
 
-listbox1 = Listbox(root)
-listbox1.grid(row =2, column=0, columnspan=2, sticky=EW)
+
+scrollbar = Scrollbar(root, orient= VERTICAL)
+listbox1 = Listbox(root, yscrollcommand=scrollbar.set)
+scrollbar.config(command=listbox1.yview) 
+scrollbar.grid(row=2, column=2, rowspan=10)
+listbox1.grid(row =2, column=0, columnspan=2, sticky=EW, rowspan=10)
 listbox1.bind("<Button-3>", clearlist)
 
-listbox1.insert(END, "Radiohead")
-listbox1.insert(END, "Beyonce")
-listbox1.insert(END, "Kendrick")
+# listbox1.insert(END, "Radiohead")
+# listbox1.insert(END, "Beyonce")
+# listbox1.insert(END, "Kendrick")
+
+size()
+
+image = Image.open("ryan2.jpg")
+image = image.resize((100,100))
+photo = ImageTk.PhotoImage(image)
+
+label2 = Label(image=photo)
+label2.image = photo # keep a reference!
+label2.grid(row=7, column =1 )
+
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Open", command=openfileR)
+filemenu.add_command(label="Save", command=openfileW)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+root.config(menu=menubar)
+
 
 mainloop() #causes the windows to display on the screen until program closes
-
 
